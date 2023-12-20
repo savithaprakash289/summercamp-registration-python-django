56c723a732301db1fa85a4033311094defe34b38
savithaprakash289 <136881941+savithaprakash289@users.noreply.github.com>
2023-12-20 22:56:50 +0530

    Update settings.py

--- summercamp/settings.py
+++ summercamp/settings.py
@@ -74,13 +74,13 @@ WSGI_APPLICATION = 'summercamp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
+# DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+#
 '''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
